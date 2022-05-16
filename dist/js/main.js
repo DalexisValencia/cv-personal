4ec7acf26b067da5db498c3344b8750136e9922a
DalexisValencia <d.alexis.valencia@gmail.com>
2022-05-16 14:40:03 -0500

    slide en mejores proyectos

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,30 @@
-(function ($) {
-    $(document).ready(function () {
-       //jquery base
+(function($) {
+    $(document).ready(function() {
+        //jquery base
+        const bestProjects = $(".owl-carousel");
+        if (bestProjects) {
+            bestProjects.owlCarousel({
+                loop: true,
+                margin: 50,
+                autoWidth: true,
+                nav: true,
+                navContainer: $(".best-proyects__carousel-nav"),
+                center: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    1000: {
+                        items: 4
+                    },
+                    1400: {
+                        items: 4
+                    }
+                }
+            })
+        }
     });
 })(jQuery);
